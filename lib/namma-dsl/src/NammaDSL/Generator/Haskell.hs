5fa9eea204e0c0627772b0517be2035ea5f7abd4
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2024-03-15 17:50:24 +0300

    fixed newtype generation

--- lib/namma-dsl/src/NammaDSL/Generator/Haskell.hs
+++ lib/namma-dsl/src/NammaDSL/Generator/Haskell.hs
@@ -1,6 +1,5 @@
 module NammaDSL.Generator.Haskell (module Reexport) where
 
--- import NammaDSL.Generator.Haskell.BeamQueries  as Reexport
 import NammaDSL.Generator.Haskell.BeamQueries as Reexport (BeamQueryCode (..), DefaultQueryCode (..), ExtraQueryCode (..), generateBeamQueries)
 import NammaDSL.Generator.Haskell.BeamTable as Reexport
 import NammaDSL.Generator.Haskell.DomainHandler as Reexport
