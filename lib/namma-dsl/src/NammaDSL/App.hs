4d1672258bf2494b87e5245137af2792b5f25ee9
ErasedSoul <anirbandas0206@gmail.com>
2024-04-02 16:51:45 +0530

    Added extra instance required for UsageSafety

--- lib/namma-dsl/src/NammaDSL/App.hs
+++ lib/namma-dsl/src/NammaDSL/App.hs
@@ -22,7 +22,7 @@ import System.Process (readProcess)
 import Prelude
 
 version :: String
-version = "1.0.22"
+version = "1.0.23"
 
 runStorageGenerator :: FilePath -> FilePath -> IO ()
 runStorageGenerator configPath yamlPath = do
