9687e3fffa42b7a122c2dac6c125ab539dc3bd17
ErasedSoul <anirbandas0206@gmail.com>
2024-04-16 18:14:26 +0530

    Enh: Added pure intermediate TFs

--- lib/namma-dsl/src/NammaDSL/Generator/Haskell/BeamQueries.hs
+++ lib/namma-dsl/src/NammaDSL/Generator/Haskell/BeamQueries.hs
@@ -5,7 +5,9 @@ module NammaDSL.Generator.Haskell.BeamQueries (generateBeamQueries, BeamQueryCod
 import Control.Lens ((%~), (.~), (^.))
 import Control.Monad (forM_, when)
 import Control.Monad.Extra (whenJust)
+import qualified Data.Text as T
 import Control.Monad.Reader (ask)
+import Control.Monad.Writer (tell)
 import Data.Bifunctor (first)
 import Data.Bool
 import Data.Function ((&))
@@ -351,7 +353,10 @@ intermediateTransformerCode itfs = do
       MonadicT -> do
           let transformerExp = vE (tfName tf)
           vP (outputVarName) <-- transformerExp
-      PureT -> pure ()
+      PureT -> do
+          let transformerExp = TH.VarE $ TH.mkName (tfName tf)
+          let valP = TH.VarP (mkNameT $ T.pack outputVarName)
+          tell [TH.LetS [TH.ValD valP (TH.NormalB transformerExp) []]]
    )
 
 -- Is it correct? toTType' is not monadic function
