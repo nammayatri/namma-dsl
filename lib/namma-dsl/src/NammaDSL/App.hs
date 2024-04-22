966945d8be75d7562a31a7c13add0fc29c61dedf
ErasedSoul <anirbandas0206@gmail.com>
2024-04-22 19:23:06 +0530

    Fix: updatedAt Maybe case

--- lib/namma-dsl/src/NammaDSL/App.hs
+++ lib/namma-dsl/src/NammaDSL/App.hs
@@ -23,7 +23,7 @@ import System.Process (readProcess)
 import Prelude
 
 version :: String
-version = "1.0.36"
+version = "1.0.37"
 
 runStorageGenerator :: FilePath -> FilePath -> IO ()
 runStorageGenerator configPath yamlPath = do
