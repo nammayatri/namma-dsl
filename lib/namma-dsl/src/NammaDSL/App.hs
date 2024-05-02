f508844ef9f36cbb6dc13358a809cbad85ac771b
ErasedSoul <anirbandas0206@gmail.com>
2024-05-02 12:33:02 +0530

    MoCity change

--- lib/namma-dsl/src/NammaDSL/App.hs
+++ lib/namma-dsl/src/NammaDSL/App.hs
@@ -23,7 +23,7 @@ import System.Process (readProcess)
 import Prelude
 
 version :: String
-version = "1.0.43"
+version = "1.0.44"
 
 runStorageGenerator :: FilePath -> FilePath -> IO ()
 runStorageGenerator configPath yamlPath = do
