24e8dc475cbe6c9e4c74afd7a92036dbe4d81611
ErasedSoul <anirbandas0206@gmail.com>
2024-05-28 18:34:10 +0530

    Build fix

--- lib/namma-dsl/src/NammaDSL/App.hs
+++ lib/namma-dsl/src/NammaDSL/App.hs
@@ -25,7 +25,7 @@ import System.Process (readProcess)
 import Prelude
 
 version :: String
-version = "1.0.50"
+version = "1.0.51"
 
 runStorageGenerator :: FilePath -> FilePath -> IO ()
 runStorageGenerator configPath yamlPath = do
