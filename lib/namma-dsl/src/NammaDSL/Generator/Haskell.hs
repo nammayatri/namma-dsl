3834b988818d70d11e68620c5e13ca93d5842d66
Roman Bodavskiy <roman.bodavskiy@metalamp.io>
2024-07-01 15:04:02 +0300

    dashboard domain handler added

--- lib/namma-dsl/src/NammaDSL/Generator/Haskell.hs
+++ lib/namma-dsl/src/NammaDSL/Generator/Haskell.hs
@@ -3,6 +3,7 @@ module NammaDSL.Generator.Haskell (module Reexport) where
 import NammaDSL.Generator.Haskell.BeamQueries as Reexport (BeamQueryCode (..), DefaultQueryCode (..), ExtraQueryCode (..), generateBeamQueries)
 import NammaDSL.Generator.Haskell.BeamTable as Reexport
 import NammaDSL.Generator.Haskell.CachedQueries as Reexport (CachedQueryCode (..), DefaultCachedQueryCode (..), ExtraCachedQueryCode (..), generateCachedQueries)
+import NammaDSL.Generator.Haskell.Dashboard.DomainHandler as Reexport
 import NammaDSL.Generator.Haskell.Dashboard.Servant as Reexport
 import NammaDSL.Generator.Haskell.DomainHandler as Reexport
 import NammaDSL.Generator.Haskell.DomainType as Reexport
