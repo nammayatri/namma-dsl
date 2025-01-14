ca9f640205d5723119710434fb12587ce76ae5f6
Shamil Nizamov <na-80@yandex.ru>
2025-01-14 12:02:41 +0500

    Add processingWhereClause function

--- lib/namma-dsl/tests/Main.hs
+++ lib/namma-dsl/tests/Main.hs
@@ -3,8 +3,14 @@
 
 module Main where
 
+--import NammaDSL.DSL.Parser.Storage (SQL_MANIPULATION, sqlCleanedLineParser,  storageParser)
+-- Temporary import. Will be deleted
+
+import Data.Default -- Temporary import. Will be deleted
 import NammaDSL.App
-import NammaDSL.DSL.Parser.Storage (SQL_MANIPULATION, sqlCleanedLineParser)
+import NammaDSL.DSL.Parser.Storage (sqlCleanedLineParser, storageParser)
+import NammaDSL.DSL.Syntax.Storage --(queries, whereClause, qpName, QueryParam) -- Temporary import. Will be deleted
+--import Data.List (isSubsequenceOf, sort) -- Temporary import. Will be deleted
 import Prelude
 
 storageYamlFilePath :: FilePath
@@ -26,4 +32,30 @@ sql :: String -> SQL_MANIPULATION -- Just for quick testing
 sql = sqlCleanedLineParser
 
 main :: IO ()
-main = pure () -- generateAllExample
+main = do
+  -- pure () -- generateAllExample
+  content <- storageParser def "./tests/storageTest.yaml"
+  mapM_ processingWhereClause content
+  putStrLn "///////////////////////////////"
+
+-- Temporary functions. Will be deleted
+-- TableDef fields :: [FieldDef], ->  beamFields :: [BeamField], ->  bConstraints :: [FieldConstraint],
+
+processingWhereClause :: TableDef -> IO ()
+processingWhereClause tableDef = do
+  let whereClauseLs = whereClause <$> queries tableDef
+      primaryKeyLs = primaryKey tableDef
+      secondaryKeyLs = secondaryKey tableDef
+      caseWhereClause whereClause' =
+        case whereClause' of
+          EmptyWhere -> putStrLn "Going on"
+          Leaf (queryParam, _) -> processingWhereClauseLeaf queryParam primaryKeyLs secondaryKeyLs
+          _ -> putStrLn "Another result"
+  mapM_ caseWhereClause whereClauseLs
+
+processingWhereClauseLeaf :: QueryParam -> [String] -> [String] -> IO ()
+processingWhereClauseLeaf queryParam primaryKeyLs secondaryKeyLs = do
+  let fildName = qpName queryParam
+  if fildName `elem` secondaryKeyLs || fildName `elem` primaryKeyLs
+    then putStrLn "Going on"
+    else putStrLn "Huston, we have a problem"
